fd4b712aad2b6bf629efb60deb59d7520257dd4d
Ruben Paz <me@ruben.io>
2022-11-22 19:33:29 +0000

    Error handling on rv32i

--- emu/src/rv32i.rs
+++ emu/src/rv32i.rs
@@ -2,20 +2,27 @@ use super::{Memory, CPU};
 use crate::format::*;
 use crate::utils::*;
 
+#[derive(Debug)]
+pub enum CPUError {
+    UnrecognizedInstruction,
+}
+
 /// Load Upper Immediate
-fn lui(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn lui(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = UFormat::try_from(word).unwrap(); // TODO: Handle trap
     cpu.x_registers[parsed.rd as usize] = sext(parsed.imm << 12, 20, 32);
+    Ok(())
 }
 
 /// Add Upper Immediate to PC
-fn auipc(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn auipc(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = UFormat::try_from(word).unwrap();
     cpu.x_registers[parsed.rd as usize] = cpu.pc + sext(parsed.imm << 12, 20, 32);
+    Ok(())
 }
 
 /// Jump and Link
-fn jal(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn jal(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = JFormat::try_from(word).unwrap();
     cpu.x_registers[parsed.rd as usize] = cpu.pc + 4;
     cpu.pc += sext(
@@ -23,17 +30,19 @@ fn jal(cpu: &mut CPU, mem: &mut Memory, word: u32) {
         20,
         32,
     );
+    Ok(())
 }
 
 /// Jump and Link Register
-fn jalr(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn jalr(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = IFormat::try_from(word).unwrap();
     cpu.pc = (cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32)) & !1;
     let addr = if parsed.rd == 0 { 1 } else { parsed.rd };
     cpu.x_registers[addr as usize] = cpu.pc + 4;
+    Ok(())
 }
 
-fn beq(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn beq(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if cpu.x_registers[word.rs1 as usize] == cpu.x_registers[word.rs2 as usize] {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -41,9 +50,10 @@ fn beq(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+    Ok(())
 }
 
-fn bge(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn bge(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if cpu.x_registers[word.rs1 as usize] as i32 >= cpu.x_registers[word.rs2 as usize] as i32 {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -51,9 +61,10 @@ fn bge(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+    Ok(())
 }
 
-fn bgeu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn bgeu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if cpu.x_registers[word.rs1 as usize] >= cpu.x_registers[word.rs2 as usize] {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -61,9 +72,10 @@ fn bgeu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+    Ok(())
 }
 
-fn blt(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn blt(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if (cpu.x_registers[word.rs1 as usize] as i32) < cpu.x_registers[word.rs2 as usize] as i32 {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -71,9 +83,10 @@ fn blt(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+    Ok(())
 }
 
-fn bltu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn bltu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if cpu.x_registers[word.rs1 as usize] < cpu.x_registers[word.rs2 as usize] {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -81,9 +94,11 @@ fn bltu(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+
+    Ok(())
 }
 
-fn bne(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
+fn bne(cpu: &mut CPU, mem: &mut Memory, word: BFormat) -> Result<(), CPUError> {
     if cpu.x_registers[word.rs1 as usize] != cpu.x_registers[word.rs2 as usize] {
         cpu.pc += sext(
             word.imm0 << 10 | word.imm1 | word.imm2 << 4 | word.imm3 << 11,
@@ -91,54 +106,61 @@ fn bne(cpu: &mut CPU, mem: &mut Memory, word: BFormat) {
             32,
         );
     }
+    Ok(())
 }
 
 /// Branch parsing
-fn branch(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn branch(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = BFormat::try_from(word).unwrap();
     let op = match parsed.funct3 {
         0b000 => beq,
-        0b101 => bge,                           // bge
-        0b111 => bgeu,                          // bgeu
-        0b100 => blt,                           // blt
-        0b110 => bltu,                          // bltu
-        0b001 => bne,                           // bne
-        _ => panic!("Unrecognized insruction"), // TODO: Trap
+        0b101 => bge,                                       // bge
+        0b111 => bgeu,                                      // bgeu
+        0b100 => blt,                                       // blt
+        0b110 => bltu,                                      // bltu
+        0b001 => bne,                                       // bne
+        _ => return Err(CPUError::UnrecognizedInstruction), // TODO: Trap
     };
-    op(cpu, mem, parsed);
+    op(cpu, mem, parsed)
 }
 
-fn lb(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lb(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = sext(mem.rb(addr as usize) as u32, 8, 32);
+    Ok(())
 }
 
-fn lbu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lbu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = mem.rb(addr as usize) as u32;
+    Ok(())
 }
 
-fn lh(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lh(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = sext(mem.rhw(addr as usize) as u32, 16, 32);
+    Ok(())
 }
 
-fn lhu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lhu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = mem.rhw(addr as usize) as u32;
+    Ok(())
 }
 
-fn lw(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lw(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = sext(mem.rw(addr as usize) as u32, 32, 32);
+    Ok(())
 }
 
-fn lwu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) {
+fn lwu(cpu: &mut CPU, mem: &mut Memory, parsed: IFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm, 12, 32);
     cpu.x_registers[parsed.rd as usize] = mem.rw(addr as usize) as u32;
+    Ok(())
 }
 
-fn load(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn load(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = IFormat::try_from(word).unwrap();
     let op = match parsed.funct3 {
         0b000 => lb,
@@ -147,40 +169,43 @@ fn load(cpu: &mut CPU, mem: &mut Memory, word: u32) {
         0b101 => lhu,
         0b010 => lw,
         0b110 => lwu,
-        _ => panic!("Unrecognized instruction"),
+        _ => return Err(CPUError::UnrecognizedInstruction),
     };
     op(cpu, mem, parsed)
 }
 
-fn sb(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) {
+fn sb(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm0 | parsed.imm1, 12, 32);
     mem.wb(addr as usize, cpu.x_registers[parsed.rs2 as usize] as u8);
+    Ok(())
 }
 
-fn sh(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) {
+fn sh(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm0 | parsed.imm1, 12, 32);
     mem.whw(addr as usize, cpu.x_registers[parsed.rs2 as usize] as u16);
+    Ok(())
 }
 
-fn sw(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) {
+fn sw(cpu: &mut CPU, mem: &mut Memory, parsed: SFormat) -> Result<(), CPUError> {
     let addr = cpu.x_registers[parsed.rs1 as usize] + sext(parsed.imm0 | parsed.imm1, 12, 32);
     mem.ww(addr as usize, cpu.x_registers[parsed.rs2 as usize]);
+    Ok(())
 }
 
-fn store(cpu: &mut CPU, mem: &mut Memory, word: u32) {
+fn store(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let parsed = SFormat::try_from(word).unwrap();
     let op = match parsed.funct3 {
         0b000 => sb,
         0b001 => sh,
         0b010 => sw,
-        _ => panic!("Unrecognized instruction"),
+        _ => return Err(CPUError::UnrecognizedInstruction),
     };
     op(cpu, mem, parsed)
 }
 
-pub fn match_rv32i(word: u32) -> () {
+pub fn rv32i(cpu: &mut CPU, mem: &mut Memory, word: u32) -> Result<(), CPUError> {
     let opcode = word & OPCODE_MASK;
-    match opcode {
+    let op = match opcode {
         0b0110111 => lui,
         0b0010111 => auipc,
         0b1101111 => jal,
@@ -192,8 +217,10 @@ pub fn match_rv32i(word: u32) -> () {
         0b0110011 => todo!(), // TODO fn3 (add, sub, sll, slt, sltu, xor, srl, sra, or, and)
         0b0001111 => todo!(), // TODO fn3 (fence, fence.i)
         0b1110011 => todo!(), // TODO fn3 (ecall, ebreak, csrrw, csrrs, csrrc, csrrwi, csrrsi, csrrci),
-        _ => panic!("Unrecognized instruction"), // TODO: Should not panic. Return Result.
+        _ => return Err(CPUError::UnrecognizedInstruction), // TODO: Should not panic. Return Result.
     };
+
+    op(cpu, mem, word)
 }
 
 #[cfg(test)]
@@ -203,7 +230,7 @@ mod tests {
     #[test]
     fn lui_writes_register() {
         let mut cpu = CPU::new();
-        let mut mem = Memory::new();
+        let mut mem = Memory::new(1024 * 100);
         // lui x1, 0x23
         let word = 0x0002_20b7;
         lui(&mut cpu, &mut mem, word);
