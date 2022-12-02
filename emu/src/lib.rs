2d783fcd74b6cdc442102ee7008981352d4f9d47
Ruben Paz <me@ruben.io>
2022-12-02 13:04:11 +0000

    Interrupt PLIC handling and other interrupt handling improvements

--- emu/src/lib.rs
+++ emu/src/lib.rs
@@ -5,7 +5,7 @@ mod utils;
 use instruction_set::{privileged::RVPrivileged, rv32i::RV32i, Instruction};
 use memory::{Memory, MMU};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Interrupt {
     SSoftInterrupt = 1,
     MSoftInterrupt = 3,
@@ -96,19 +96,16 @@ impl Emulator {
         Ok(v)
     }
 
+    // TODO Rename
     fn generate_clint_interrupts(&mut self) {
         use Interrupt::*;
-        let mie = self.cpu.get_csr(CSRs::mie as u32).unwrap();
-        let msi = (mie & (1 << MSoftInterrupt as u32)) != 0;
         let software_interrupt = self.mem.rw(0x200_0000).unwrap();
-        if msi && software_interrupt > 0 {
+        if software_interrupt > 0 {
             let mip = self.cpu.get_csr(CSRs::mip as u32).unwrap();
             let mip = mip | (1 << MSoftInterrupt as u32);
             self.cpu.set_csr(CSRs::mip as u32, mip).unwrap();
             self.mem.ww(0x200_0000, 0).unwrap();
         }
-
-        let mti = (mie & (1 << MTimerInterrupt as u32)) != 0;
 
         let cmp_time: u64 = self.mem.rw(0x200_4000).unwrap() as u64;
 
@@ -118,7 +115,7 @@ impl Emulator {
         let time: u64 = time | ((self.mem.rw(0x200_bffc).unwrap() as u64) << 4);
         let time = time + 1;
 
-        if mti && time >= cmp_time {
+        if cmp_time != 0 && time >= cmp_time {
             let mip = self.cpu.get_csr(CSRs::mip as u32).unwrap();
             let mip = mip | (1 << MTimerInterrupt as u32);
             self.cpu.set_csr(CSRs::mip as u32, mip).unwrap();
@@ -130,20 +127,39 @@ impl Emulator {
         self.mem.ww(0x200_bffc, time32).unwrap();
     }
 
+    // TODO: Rename
+    fn gen_plic_interrupts(&mut self) {
+        let external_interrupts =
+            self.mem.rw(0x1000).unwrap() as u64 | ((self.mem.rw(0x1004).unwrap() as u64) << 4);
+        if external_interrupts != 0 {
+            let mip = self.cpu.get_csr(CSRs::mip as u32).unwrap();
+            let mip = mip | (1 << Interrupt::MExternalInterrupt as u32);
+            self.cpu.set_csr(CSRs::mip as u32, mip).unwrap();
+        }
+    }
+
     fn get_interrupt(&mut self) -> Option<Interrupt> {
         use Interrupt::*;
+        let mie = self.cpu.get_csr(CSRs::mie as u32).unwrap();
+        let mie_msi = (mie & (1 << MSoftInterrupt as u32)) != 0;
+        let mie_mti = mie & (1 << MTimerInterrupt as u32) != 0;
+        let mie_mei = mie & (1 << MExternalInterrupt as u32) != 0;
+
         let mip = self.cpu.get_csr(CSRs::mip as u32).unwrap();
-        let msi = mip & (1 << MSoftInterrupt as u32);
+        let mip_msi = mip & (1 << MSoftInterrupt as u32) != 0;
+        let mip_mti = mip & (1 << MTimerInterrupt as u32) != 0;
+        let mip_mei = mip & (1 << MExternalInterrupt as u32) != 0;
+
         match mip {
             // TODO: Handle interrupts for other privilege modes
-            _v if msi != 0 => {
+            _v if mie_msi && mip_msi => {
                 self.cpu
                     .set_csr(CSRs::mip as u32, mip & !(1 << MSoftInterrupt as u32))
                     .unwrap();
                 Some(MSoftInterrupt)
             }
-            v if (v & (1 << MTimerInterrupt as u32)) != 0 => Some(MTimerInterrupt),
-            v if (v & (1 << MExternalInterrupt as u32)) != 0 => Some(MExternalInterrupt),
+            _v if mie_mti && mip_mti => Some(MTimerInterrupt),
+            _v if mie_mei && mip_mei => Some(MExternalInterrupt),
             _ => None,
         }
     }
@@ -157,6 +173,7 @@ impl Emulator {
         let mut interrupt = None;
         if mstatus_mie {
             self.generate_clint_interrupts();
+            self.gen_plic_interrupts();
             interrupt = self.get_interrupt();
         }
 
@@ -186,6 +203,7 @@ impl Emulator {
 
         let mstatus_mie = mstatus & (1 << 3);
         let mie = self.cpu.get_csr(CSRs::mie as u32).unwrap();
+        let mip = self.cpu.get_csr(CSRs::mip as u32).unwrap();
 
         let _mcause = match exc {
             ExceptionInterrupt::Interrupt(i) => {
@@ -193,6 +211,18 @@ impl Emulator {
                 if mstatus_mie != 0 && (mie & (1 << i as u32) != 0) {
                     self.cpu
                         .set_csr(CSRs::mcause as u32, i as u32 | (1 << 31))
+                        .unwrap();
+                    if i == Interrupt::MExternalInterrupt {
+                        // XXX: For external interrupts, this implementation resets the PLIC pending
+                        // bit, and sets the interrupt value to mtval which may not be the correct vehaviour.
+                        let plic_int = self.mem.rw(0x20_0004).unwrap();
+                        self.cpu.set_csr(CSRs::mtval as u32, plic_int).unwrap();
+                        self.mem.ww(0x20_0004, plic_int).unwrap();
+                    }
+                    // XXX: This implementation reset the pending interrupt bit before the handler
+                    // executes it, I'm not sure this is the correct behaviour
+                    self.cpu
+                        .set_csr(CSRs::mip as u32, mip & !(i as u32))
                         .unwrap();
                 } else {
                     return TickResult::Cycles(4);
@@ -204,7 +234,12 @@ impl Emulator {
                     Exception::MEnvironmentCall if self.cpu.x[15] == 255 => {
                         return TickResult::HALT
                     }
-                    _ => self.cpu.set_csr(CSRs::mcause as u32, e as u32).unwrap(),
+                    _ => {
+                        // XXX: Exceptions add the pc to the mtval, which may not be the correct
+                        // way to pass the pc to the handler
+                        self.cpu.set_csr(CSRs::mcause as u32, e as u32).unwrap();
+                        self.cpu.set_csr(CSRs::mtval as u32, self.cpu.pc).unwrap();
+                    }
                 };
                 e as u32
             }
@@ -213,8 +248,7 @@ impl Emulator {
         let mstatus = (mstatus & !(1 << 3)) | mstatus_mie << 4; // move mie to mpie and set mie to 0 (disable interrupts)
         self.cpu.set_csr(CSRs::mstatus as u32, mstatus).unwrap();
         self.cpu.set_csr(CSRs::mepc as u32, self.cpu.pc).unwrap();
-        self.cpu.set_csr(CSRs::mtval as u32, self.cpu.pc).unwrap(); // TODO: How do we bubble up suporting data?
-                                                                    // TODO: Set privilege mode on msatus.MPP
+        // TODO: Set privilege mode on msatus.MPP
         let mtvec = self.cpu.get_csr(CSRs::mtvec as u32).unwrap();
         self.cpu.pc = mtvec;
         TickResult::Cycles(4)
