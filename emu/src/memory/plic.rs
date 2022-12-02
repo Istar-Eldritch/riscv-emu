cfc1f2e5da02049208de31399b797d8e54c44f09
Ruben Paz <me@ruben.io>
2022-12-02 15:33:12 +0000

    Fix behaviour of PLIC

--- emu/src/memory/plic.rs
+++ emu/src/memory/plic.rs
@@ -1,8 +1,9 @@
 use super::{Memory, MemoryError};
+use std::cell::RefCell;
 
 pub struct PLIC {
     pub source_priority: [u32; 52], // addr 0x0
-    pub pending: u64,               // addr 0xd0
+    pub pending: RefCell<u64>,      // addr 0xd0
     pub h0mie: u64,                 // hart0 M-Mode interrupt enables - addr 216
     pub h0mpt: u32,                 // hart0 M-Mode priority threshold - addr 224
     pub h0mcc: u32,                 // hart0 M-Mode claim/complete - addr 228
@@ -12,7 +13,7 @@ impl PLIC {
     pub fn new() -> Self {
         Self {
             source_priority: [0; 52],
-            pending: 0,
+            pending: RefCell::new(0),
             h0mie: 0,
             h0mpt: 0,
             h0mcc: 0,
@@ -20,12 +21,15 @@ impl PLIC {
     }
 
     // Returns the enabled and pending interrupts ordered by priority
-    pub fn get_interrupts(&self) -> Vec<u32> {
+    fn get_interrupts(&self) -> Vec<u32> {
         let mut interrupts = Vec::new();
         for i in 0..52 {
             let code = 1 << i;
             let priority = self.source_priority[code as usize];
-            if self.h0mie & code != 0 && self.pending & code != 0 && priority >= self.h0mpt {
+            if self.h0mie & code != 0
+                && *self.pending.borrow() & code != 0
+                && priority >= self.h0mpt
+            {
                 interrupts.push(i);
             }
         }
@@ -39,7 +43,8 @@ impl PLIC {
 
     pub fn interrupt(&mut self, v: u64) {
         // XXX: if the interrupt is enabled should we ignore adding the pending bit?
-        self.pending = self.pending | v;
+        let mut pending = self.pending.borrow_mut();
+        *pending = *pending | v;
     }
 }
 
@@ -64,7 +69,7 @@ impl Memory for PLIC {
         match addr {
             v if v >= 0x4 && v < 0xd4 => Ok(self.source_priority[((v - 4) / 4) as usize]),
             v if v == 0x1000 && v == 0x1004 => {
-                let pending: *const u64 = &self.pending;
+                let pending: *const u64 = &*self.pending.borrow();
                 let shift = v - 0x1000;
                 let to_read: *const u32 = ((pending as u32) + shift) as *const u32;
                 unsafe { Ok(*to_read) }
@@ -79,7 +84,10 @@ impl Memory for PLIC {
             0x20_0004 => {
                 // XXX: Should we keep a list of claimed interrupts?
                 let mut interrupts = self.get_interrupts();
-                Ok(interrupts.pop().unwrap_or(0))
+                let interrupt = interrupts.pop().unwrap_or(0);
+                let mut pending = self.pending.borrow_mut();
+                *pending = *pending & !(1 << interrupt);
+                Ok(interrupt)
             }
             _ => Err(MemoryError::AccessFault),
         }
@@ -92,7 +100,7 @@ impl Memory for PLIC {
                 Ok(())
             }
             v if v == 0x1000 && v == 0x1004 => {
-                let pending: *const u64 = &self.pending;
+                let pending: *const u64 = &*self.pending.borrow();
                 let shift = v - 0x1000;
                 let to_write: *mut u32 = ((pending as u32) + shift) as *mut u32;
                 unsafe { *to_write = value };
@@ -110,8 +118,7 @@ impl Memory for PLIC {
                 Ok(())
             }
             0x20_0004 => {
-                // Just disabled the interrupt
-                self.pending = self.pending & !(1 << value);
+                // XXX: Not sure what to do here. Should we keep a list of the claimed interrupts?
                 Ok(())
             }
             _ => Err(MemoryError::AccessFault),
